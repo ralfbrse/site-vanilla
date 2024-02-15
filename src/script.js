789753ff7fcdb987a217651ddffe627394756215
ralfbrse <raphaeloburce@gmail.com>
2024-02-15 01:14:45 -0800

    Completed education timeline

--- src/script.js
+++ src/script.js
@@ -12,26 +12,27 @@ document.addEventListener("mousemove", (event) => {
   mouseX = event.clientX;
   mouseY = event.clientY;
 });
+if (beef_sprite) {
+  setInterval(() => {
+    beef_sprite.style.left = beefX + dx + "px";
+    beef_sprite.style.top = beefY + dy + "px";
+    dx = (mouseX - beefX) * speed;
+    dy = (mouseY - beefY) * speed;
 
-setInterval(() => {
-  beef_sprite.style.left = beefX + dx + "px";
-  beef_sprite.style.top = beefY + dy + "px";
-  dx = (mouseX - beefX) * speed;
-  dy = (mouseY - beefY) * speed;
+    beefX = beef_sprite.offsetLeft;
+    beefY = beef_sprite.offsetTop;
 
-  beefX = beef_sprite.offsetLeft;
-  beefY = beef_sprite.offsetTop;
+    if (dx > 0) {
+      beef_sprite.style.transform = "scaleX(-1)";
+    } else {
+      beef_sprite.style.transform = null;
+    }
 
-  if (dx > 0) {
-    beef_sprite.style.transform = "scaleX(-1)";
-  } else {
-    beef_sprite.style.transform = null;
-  }
+    console.log("moving");
+    console.log("beefx = ", beefX);
+    console.log("adding...", dx);
 
-  console.log("moving");
-  console.log("beefx = ", beefX);
-  console.log("adding...", dx);
-
-  //If dx or dy < 0.3, change to play sprite
-  //if above, change to walk sprite
-}, 30);
+    //If dx or dy < 0.3, change to play sprite
+    //if above, change to walk sprite
+  }, 30);
+}
